d350632470785ad9f0df83ebee57ea5e4eed1e0b
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-06-29 22:46:54 +0530

    addeduc6

--- bridzelabs/employeeWage.java
+++ bridzelabs/employeeWage.java
@@ -1,32 +1,36 @@
 package com.bridzelabs;
 import java.util.Random;
 public class employeeWage {
+
+
         public static void main(String[] args){
             System.out.println("Welcom to Employee Wage Computation Program");
             Random random = new Random();
-            int attendance = random.nextInt(2);
             int wagePerHour=20;
             int fullDayHours=8;
             int partTimeHours=4;
             int wagePerDay = 0;
+            int monthlySalary = 0;
+            int monthlyHours = 0;
+            int days = 0;
+            while (monthlyHours<= 100 && days<= 20){
 
-            switch(attendance)
-            {
-                case 0: System.out.println("Employee Absent..");
-                    break;
-                case 1: wagePerDay = partTimeHours * wagePerHour;
-                    break;
-                default:
-                    wagePerDay = fullDayHours * wagePerHour;
+                int attendance = random.nextInt(3);
+                wagePerDay=0;
+                switch (attendance) {
+                    case 0:
+                        System.out.println("Employee Absent..");
+                        monthlyHours=monthlyHours + partTimeHours;
+                        break;
+                    case 1:
+                        wagePerDay = partTimeHours * wagePerHour;
+                        break;
+                    default:
+                        wagePerDay = fullDayHours * wagePerHour;
+                        monthlyHours = monthlyHours + fullDayHours;
+                }
+                monthlySalary = monthlySalary + wagePerDay ;
             }
-
-            if(attendance == 1) {
-                System.out.println("Employee present...");
-                wagePerDay = wagePerHour * fullDayHours;
-            }
-            else
-                System.out.println("Employee Absent....");
-            System.out.println("Employee wages Per Day: " + wagePerDay);
 
 
         }
