045f15acda495b18399f5cc0009ceebdb92b0739
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-06-29 21:56:19 +0530

    addeduc1

---
+++ bridzelabs/employeeWage.java
@@ -0,0 +1,10 @@
+package com.bridzelabs;
+
+public class employeeWage {
+
+        public static void main(String[] args){
+            System.out.println("Welcom to Employee Wage Computation Program");
+        }
+    }
+
+
