1ed92d1ee15adc589b49f1f06045ee4fb15cfa02
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-06-29 22:15:05 +0530

    addeduc3

--- bridzelabs/employeeWage.java
+++ bridzelabs/employeeWage.java
@@ -3,18 +3,36 @@ import java.util.Random;
 public class employeeWage {
 
 
+    public static void main(String[] args) {
+        System.out.println("Welcom to Employee Wage Computation Program");
+        Random random = new Random();
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int partTimeHours = 4;
+        int wagePerDay = 0;
+        int monthlySalary = 0;
 
-
-        public static void main(String[] args){
-            System.out.println("Welcom to Employee Wage Computation Program");
-            Random random = new Random();
-            int attendance = random.nextInt(2);
-            if(attendance == 1)
-                System.out.println("Employee present...");
-            else
-                System.out.println("Employee Absent....");
-
-
+        for (int day = 1; day <= 20; day++) {
+            int attendance = random.nextInt(3);
+            wagePerDay = 0;
+            switch (attendance) {
+                case 0:
+                    System.out.println("Employee Absent..");
+                    break;
+                case 1:
+                    wagePerDay = partTimeHours * wagePerHour;
+                    break;
+                default:
+                    wagePerDay = fullDayHours * wagePerHour;
+            }
+            monthlySalary = monthlySalary + wagePerDay;
         }
     }
+}
+
+
+
+
+
+
 
