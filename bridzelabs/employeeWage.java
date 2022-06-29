c57d6fb47cd802a588b54431fd10f93315f795c5
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-06-29 22:05:55 +0530

    addeduc2

--- bridzelabs/employeeWage.java
+++ bridzelabs/employeeWage.java
@@ -1,10 +1,20 @@
 package com.bridzelabs;
-
+import java.util.Random;
 public class employeeWage {
+
+
+
 
         public static void main(String[] args){
             System.out.println("Welcom to Employee Wage Computation Program");
+            Random random = new Random();
+            int attendance = random.nextInt(2);
+            if(attendance == 1)
+                System.out.println("Employee present...");
+            else
+                System.out.println("Employee Absent....");
+
+
         }
     }
-
 
